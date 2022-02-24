55213c520c6580b635328e1108c5e3466f862d61
Renjith Devassy <renjithdevassy@gmail.com>
2022-02-24 12:15:23 +0000

    Add project files.

---
+++ MoviesLibrary.Web.API/Controllers/CommonController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace MoviesLibrary.Web.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CommonController :  ControllerBase
+    {
+        private readonly ILogger<CommonController> _logger;
+
+        public CommonController(ILogger<CommonController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet(Name = "GetUser")]
+        public User Get()
+        {
+            User user = new User();
+            using (StreamReader r = new StreamReader("Data/user.json"))
+            {
+                string json = r.ReadToEnd();
+                User item = JsonSerializer.Deserialize<User>(json);
+                return item;
+            }
+            return user;
+
+        }
+    }
+}
---
+++ MoviesLibrary.Web.API/User.cs
@@ -0,0 +1,15 @@
+﻿namespace MoviesLibrary.Web.API
+{
+    public class User
+    {
+        public List<UserModel> users { get; set; }
+    }
+    public class UserModel
+    {
+        public int id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+
+    }
+}
